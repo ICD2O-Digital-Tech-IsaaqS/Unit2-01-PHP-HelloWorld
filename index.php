cffa81c1db2d61f005eba60bd4dd361aabeb895f
Isaaq Simon <IsaaqS>
2025-03-19 14:16:09 +0000

    PHP

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 	</head>
 	<body>
     <h1><?php echo 'Hello, World! in PHP.'; ?></h1>
-		<?php echo '<p>This is my first web page using PHP and can already tell that I am going to hate PHP.</p>'; ?>
+		<?php echo '<p>This is my first web page using PHP and I can already tell that I am going to hate PHP.</p>'; ?>
 	</body>
 </html>
